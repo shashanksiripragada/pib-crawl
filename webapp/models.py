4b997852305e1cc7b1d464efed239037f9c3c5b8
Jerin Philip <jerin.philip@research.iiit.ac.in>
2019-09-18 03:58:01 +0530

    Adding templates and group model

--- webapp/models.py
+++ webapp/models.py
@@ -2,18 +2,21 @@ from . import db
 import datetime
 
 class Entry(db.Model):
-   id = db.Column('id', db.Integer, primary_key = True)
-   lang = db.Column(db.String(100))
-   date = db.Column(db.DateTime, default=datetime.datetime.utcnow,index=True)
-   content = db.Column(db.Text)
-   city = db.Column(db.String(100))
-'''
-class Mapping(db.Model):
-   id = db.Column('id', db.Integer, primary_key = True)
-   lang = db.Column(db.String(100))
-   date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-   city = db.Column(db.String(100))
-'''
+    __tablename__ = 'entry'
+    id = db.Column('id', db.Integer, primary_key = True)
+    lang = db.Column(db.String(100))
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow, index=True)
+    content = db.Column(db.Text)
+    city = db.Column(db.String(100))
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=True)
+
+
+
+class Group(db.Model):
+    __tablename__ = 'group'
+    id = db.Column('id', db.Integer, primary_key = True)
+    entries = db.relationship('Entry', backref='group', lazy=True)
+
 db.create_all()
 
 
