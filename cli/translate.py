6fbb48b5a74e7094be6db3d18ac1b44244aafdd7
Shashank Siripragada <siripragadashashank@gmail.com>
2019-11-06 03:59:54 +0530

    adding batched translation for ilmulti

--- cli/translate.py
+++ cli/translate.py
@@ -7,8 +7,11 @@ from ilmulti.segment import Segmenter
 from ilmulti.sentencepiece import SentencePieceTokenizer
 from ilmulti.translator.pretrained import mm_all
 from tqdm import tqdm
-from utils import process, create_stringio
+from argparse import ArgumentParser
+#from utils import process, create_stringio
 from ilmulti.utils.language_utils import inject_token
+from utils import BatchBuilder
+from collections import defaultdict
 
 segmenter = Segmenter()
 tokenizer = SentencePieceTokenizer()
@@ -20,34 +23,51 @@ langs = ['hi','ml','bn','te','ta','ur']
 model = 'mm_all'
 error = open('translate_error.txt','w+')
 
-def translate():        
+def translate(max_tokens):        
     entries = db.session.query(Entry.id,Entry.lang,Entry.date,Entry.content)\
                 .filter(Entry.lang.in_(langs)).all()
     tgt_lang = 'en'
-    for entry in tqdm(entries):
-        if entry.content:
-            exists = Translation.query.filter(Translation.parent_id==entry.id).first()
-            if not exists:
-                '''
-                src_tok = inject_lang_token(entry.content,tgt_lang=tgt_lang)
-                out = translator(src_tok)
-                hyps = [ gout['tgt'] for gout in out ]
-                hyps = detok(hyps)
-                translated = '\n'.join(hyps)
-                '''
-                src_tokenized, src_io = process(entry.content, entry.lang)
-                injected_src_tokenized = inject_token(src_tokenized, tgt_lang)
+    batches = BatchBuilder(entries, max_tokens, tgt_lang)
+    for batch in tqdm(batches):
+        generation_output = translator(batch.lines)        
+        hyps = [ gout['tgt'] for gout in generation_output ]
+        ids = [ gout['id'] for gout in generation_output ]
+        mapping = defaultdict(list)
+        for uid in batch.uids:
+            uid = uid.split()
+            mapping[int(uid[0])].append(int(uid[1]))
 
-                generation_output = self.model(injected_src_tokenized)
+        start = 0 
+        for entry_id in mapping:
+            num_lines = len(mapping[entry_id])
+            translated = hyps[start:start+num_lines]
+            print(translated)
+            start = num_lines
 
-                hyps = [ gout['tgt'] for gout in generation_output ]
+            entry = Translation(parent_id= entry_id, model= model, lang= tgt_lang, translated= translated)
+            try:
+                db.session.add(entry)
+                db.session.commit()
+            except:
+                print(entry.id,fp=error)
 
-                entry = Translation(parent_id= entry.id, model= model, lang= tgt_lang, translated= translated)
-                try:
-                    db.session.add(entry)
-                    db.session.commit()
-                except:
-                    print(entry.id,fp=error)
+        #print(ids,batch.uids)
+        #translated = '\n'.join(hyps)
+        #print(translated)
+
 
 if __name__ == '__main__':
-    translate()
+    parser=ArgumentParser()
+    parser.add_argument('max_tokens',type=int)
+    args = parser.parse_args()
+    translate(args.max_tokens)
+
+
+
+
+#if entry.content:
+#    exists = Translation.query.filter(Translation.parent_id==entry.id).first()
+#    if not exists:
+#        src_tokenized, src_io = process(entry.content, entry.lang)
+#        injected_src_tokenized = inject_token(src_tokenized, tgt_lang)
+
--- cli/utils.py
+++ cli/utils.py
@@ -6,6 +6,87 @@ from ilmulti.sentencepiece import SentencePieceTokenizer
 segmenter = Segmenter()
 tokenizer = SentencePieceTokenizer()
 
+
+class Batch:
+    def __init__(self, uids, lines):
+        #self.uids = uids
+        self.uids = uids
+        self.lines = lines
+        self.target = None
+
+    def set_target(self, target):
+        self.target = target   
+
+class BatchBuilder:
+    def __init__(self, entries, max_tokens, tgt_lang, max_lines=None):
+        self.entries = entries
+        self.max_tokens = max_tokens
+        self.tgt_lang = tgt_lang
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        # Return a single batch
+        batch = self.next_batch()
+        return batch
+    
+    def count_tokens(self, lines):
+        content_length = 0
+        for line in lines:
+            line = line.split()
+            content_length += len(line)
+        return content_length
+
+    def create_stringio(self, lines, lang):
+        tokenized = [ ' '.join(tokenizer(line, lang=lang)[1]) \
+                for line in lines ]
+        lstring = '\n'.join(tokenized)
+        return tokenized, StringIO(lstring)
+
+    def process(self, content, lang):
+        lang, segments = segmenter(content, lang=lang)
+        tokenized, _io = self.create_stringio(segments, lang)
+        return tokenized, _io
+
+
+    def get_entry(self, entry):
+        uids, lines = [], []
+        tokenized_lines ,_ = self.process(entry.content, entry.lang)
+        injected_lines = inject_token(tokenized_lines, self.tgt_lang)
+        uid_list = ['{} {}'.format(entry.id, count) for count, line in enumerate(injected_lines) ]
+        max_len = max([ len(line.split()) for line in injected_lines ])
+        token_count = self.count_tokens(injected_lines)
+        return uid_list, injected_lines, max_len, token_count
+
+
+    def next_batch(self):
+        uids, lines = [], []
+        current_tokens = 0
+        max_length = 0
+        tokens = 0
+        sizes = []
+        while(current_tokens < self.max_tokens):
+            entry = self.entries[self.index]
+            if entry.content:
+                _uids, _lines, max_len, token_count = self.get_entry(entry)
+                uids.extend(_uids)
+                lines.extend(_lines)
+                max_length = max(max_length, max_len)
+                #tokens += token_count
+                current_tokens += max_length * len(lines)
+            self.index = self.index + 1
+            if self.index > len(self.entries):
+                break
+        return Batch(uids, lines)
+
+
+'''
+def collect(batches):
+    list of entries, ready to be written
+'''      
+
 def inject_lang_token(content, tgt_lang):
     lang, content = segmenter(content)
     output = []
