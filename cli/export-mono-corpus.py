e86930762e9a08794cee0791a1b0a9785beec336
Jerin Philip <jerin.philip@research.iiit.ac.in>
2020-03-04 02:09:12 +0530

    adds code to extract monolingual corpus

---
+++ cli/export-mono-corpus.py
@@ -0,0 +1,53 @@
+import sys
+import numpy as np
+sys.path.insert(1, '../')
+from webapp import db
+from io import StringIO
+from webapp.models import Entry, Link, Translation, Retrieval
+from ilmulti.segment import Segmenter
+from ilmulti.sentencepiece import SentencePieceTokenizer
+from ilmulti.translator.pretrained import mm_all
+from bleualign.align import Aligner
+from utils import Preproc
+from tools.align import BLEUAligner
+from tqdm import tqdm
+from argparse import ArgumentParser
+from sqlalchemy import func, and_
+from urduhack.tokenization import sentence_tokenizer
+langs = ['hi', 'ta', 'te', 'ml', 'ur', 'bn', 'gu', 'mr', 'pa', 'or']
+
+segmenter = Segmenter()
+# tokenizer = SentencePieceTokenizer()
+# root = '/home/darth.vader/.ilmulti/mm-all'
+
+# preproc = Preproc(segmenter, tokenizer)
+
+
+def paralle_write(src_entry, src_lang):
+    #print('##########Article {} #########'.format(q_id),file=src_file)
+    print(src_entry,file=src_file)
+
+
+
+def export(src_lang):
+    entries = db.session.query(Entry).filter(Entry.lang==src_lang).all()
+    #entry_list = [entry.id for entry in entries]
+    for entry in tqdm(entries):
+        print(entry.id)
+        lang, segments = segmenter(entry.content, lang=src_lang)
+        output = '\n'.join(segments)
+        print(output)
+        break
+
+
+if __name__ == '__main__':
+    parser=ArgumentParser()
+    parser.add_argument('src_lang', help='language of mono corpus')
+    #parser.add_argument('tgt_lang', help='english is the target language')
+    args = parser.parse_args()
+    #src_lang, tgt_lang = args.src_lang, args.tgt_lang
+    src_lang = args.src_lang
+    src_file = open('pib_mono.{}.txt'.format(src_lang),'a')
+    #tgt_file = open('pib_en-{}.{}.txt'.format(src_lang, tgt_lang),'a')
+    export(src_lang)
+
