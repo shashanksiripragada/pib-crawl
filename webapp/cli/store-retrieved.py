5525c089263a7384f9e6f71423390320ff877293
Shashank Siripragada <siripragadashashank@gmail.com>
2020-06-27 23:05:05 +0530

    cleaning up store-retrieved

--- webapp/cli/store-retrieved.py
+++ webapp/cli/store-retrieved.py
@@ -1,7 +1,5 @@
 import os
 import sys
-sys.path.insert(1, os.getcwd())
-sys.path.insert(1, '../')
 from tqdm import tqdm
 from webapp import db
 from webapp.models import Entry, Link, Translation, Retrieval
@@ -10,37 +8,53 @@ from sqlalchemy import func, and_
 from argparse import ArgumentParser
 
 
-def store_retrieved(model, langs):
+def store_retrieved(model, langs):    
     
-    reqs = db.session.query(Entry.id).filter(Entry.lang.in_(langs)).all()
-    reqs = [req.id for req in reqs]
-    queries = db.session.query(Translation)\
-                        .filter(and_(Translation.model==model, Translation.parent_id.in_(reqs)))\
-                        .all()
-    for q in tqdm(queries):
-        if q.translated:
-            exists = Retrieval.query.filter(and_(Retrieval.query_id==q.parent_id,\
-                                         Retrieval.model==model))\
-                                         .first()
+    entries = Entry.query.filter(
+                    Entry.lang.in_(langs)
+                ).all()
+
+    entry_ids = [entry.id for entry in entries]
+    
+    queries =   Translation.query.filter(
+                    and_(
+                        Translation.model==model, 
+                        Translation.parent_id.in_(entry_ids)
+                    )
+                ).all()
+
+    for query in tqdm(queries):
+        if query.translated:
+            exists = Retrieval.query.filter(
+                        and_(
+                            Retrieval.query_id==query.parent_id,
+                            Retrieval.model==model
+                        )
+                    ).first()
+
             if not exists:
                 try:
-                    retrieved = retrieve_neighbours_en(q.parent_id, model)
+                    retrieved = retrieve_neighbours_en(query.parent_id, model)
                 except:
-                    print(q.parent_id,file=error)
+                    print(query.parent_id,file=error)
                     continue
                 else:
                     retrieved_id = retrieved[0][0]
                     score = retrieved[0][1]   
-                    entry = Retrieval(query_id=q.parent_id, retrieved_id=retrieved_id,\
-                                      score=score, model=model)
+                    entry = Retrieval(
+                                query_id=query.parent_id, 
+                                retrieved_id=retrieved_id,
+                                score=score, 
+                                model=model
+                            )
                     try:
                         db.session.add(entry)
                         db.session.commit()
                     except:
-                        print(q.parent_id,file=error)
+                        print(query.parent_id,file=error)
 
 if __name__ == '__main__':
-    langs = ['hi', 'ta', 'te', 'ml', 'bn', 'gu', 'mr', 'pa', 'or'] #[ur]
+    langs = ['hi', 'ta', 'te', 'ml', 'bn', 'gu', 'mr', 'pa', 'or', 'ur']
     parser=ArgumentParser()
     parser.add_argument('--model', help='retrieval based on model used for tanslation', required=True)
     args = parser.parse_args()
