d58473f0a49817dadf8c46346a423bb8eb4b15dc
Jerin Philip <jerin.philip@research.iiit.ac.in>
2020-06-27 11:15:53 +0200

    Moving cli inside

--- webapp/lazy_load.py
+++ webapp/lazy_load.py
@@ -1,16 +1,19 @@
-from cli import ILMULTI_DIR
+
+translator = None
 
 def get_translator():
-    from ilmulti.translator.pretrained import mm_all
-    root = os.path.join(ILMULTI_DIR, 'mm-all')
-    translator = mm_all(root=root, use_cuda=True).get_translator()
+    from ilmulti.translator import from_pretrained
+    if translator is None:
+        translator = from_pretrained(tag='mm-all-iter0').translator
     return translator
 
 def get_aligner():
+    translator = get_translator()
     aligner = BLEUAligner(translator, tokenizer, segmenter)
     return aligner
 
 def get_tokenizer():
     from ilmulti.sentencepiece import build_tokenizer
-    tokenizer = build_tokenizer()
+    tokenizer = build_tokenizer('ilmulti-v1')
+    return tokenizer
 
