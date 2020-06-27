c9c680b1883052132368551bcde5e8ac9a7a476c
Jerin Philip <jerin.philip@research.iiit.ac.in>
2020-06-27 17:54:47 +0200

    Local changes

--- webapp/utils.py
+++ webapp/utils.py
@@ -12,3 +12,9 @@ def split_and_wrap_in_p(text):
     wrap = lambda l: '<p>{}</p>'.format(l)
     lines = list(map(wrap, lines))
     return '\n'.join(lines)
+
+def clean_translation(tokenizer, translation):
+    lines = translation.translated.splitlines()
+    detokenized = detok(tokenizer, lines)
+    translated_text = '\n'.join(detokenized)
+    return translated_text
