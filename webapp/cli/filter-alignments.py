e9986f4a72b9625c4cba925d5e25614f2f779546
Shashank Siripragada <siripragadashashank@gmail.com>
2020-07-01 19:58:07 +0530

    cleaned up filter-alignemnts code

--- webapp/cli/filter-alignments.py
+++ webapp/cli/filter-alignments.py
@@ -1,23 +1,13 @@
-import os
-import sys
-sys.path.insert(1, os.getcwd())
-sys.path.insert(1, '../')
 from io import StringIO
-from ilmulti.segment import Segmenter
-from ilmulti.sentencepiece import SentencePieceTokenizer
 from tqdm import tqdm
 from argparse import ArgumentParser
+
+from ilmulti.translator import from_pretrained
+from .utils import Preproc
+
 import langid
-from utils import Preproc
-from langid.langid import LanguageIdentifier, model
-identifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
-
-#langs = ['en', 'ml', 'ur', 'te', 'hi', 'pa', 'kn', 'or', 'as', 'gu', 'mr', 'ta', 'bn']
-
-segmenter = Segmenter()
-tokenizer = SentencePieceTokenizer()
-preproc = Preproc(segmenter, tokenizer)
-
+from langid.langid import LanguageIdentifier
+from langid.langid import model as m
 
 def parallel_write(src_lang, src_entry, tgt_lang, tgt_entry):
     src_out = open('train.en-{}.{}'.format(src_lang, src_lang),'a')
@@ -29,8 +19,8 @@ def eval_len_ratio(src_len, tgt_len):
     if src_len==0 or tgt_len==0:
         return False
     ratio = src_len/tgt_len
-    src = (src_len >=2)#(2 <= src_len <= 50)
-    tgt = (tgt_len >=2)#(2 <= tgt_len <= 50)
+    src = (src_len >=2)
+    tgt = (tgt_len >=2)
     if 0.5 <= ratio <= 2 and src and tgt:
         return True
 
@@ -61,20 +51,24 @@ def filter_lines(src_lang, src_file, tgt_lang, tgt_file):
                 tgt_uniq.add(tgt_line)
                 parallel_write(src_lang, src_line, tgt_lang, tgt_line)
 
-
-
 if __name__ == '__main__':
     parser=ArgumentParser()
-    parser.add_argument('src_lang', help='non-english language')
-    parser.add_argument('tgt_lang', help='english is the target language')
-    #parser.add_argument('src_file', help='src file')
-    #parser.add_argument('tgt_file', help='src file to write the alignments into')
+    parser.add_argument('--src_lang', help='source language, non-english', required=True)
+    parser.add_argument('--tgt_lang', help='target language', required=True )
+    parser.add_argument('--model', help='translation model for generating dataset', required=True)
+
+    # engine = from_pretrained(tag=model, use_cuda=False)
+    # preproc = Preproc(engine.segmenter, engine.tokenizer)
+
     args = parser.parse_args()
     src_lang, tgt_lang = args.src_lang, args.tgt_lang
-    model = 'mm_all_iter1'
+    model = args.model
+
     src_file = open('pib_{}_en-{}.{}.txt'.format(model, src_lang, src_lang),'r')
     tgt_file = open('pib_{}_en-{}.{}.txt'.format(model, src_lang, tgt_lang),'r')
 
+    identifier = LanguageIdentifier.from_modelstring(m, norm_probs=True)
     identifier.set_languages(['{}'.format(src_lang),'{}'.format(tgt_lang)])
+    
     filter_lines(src_lang, src_file, tgt_lang, tgt_file)
 
