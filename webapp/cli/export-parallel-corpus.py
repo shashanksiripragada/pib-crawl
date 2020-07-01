b7bf5bd40b4c755a93ec18402228920f44ffd5f9
Shashank Siripragada <siripragadashashank@gmail.com>
2020-07-01 15:27:13 +0530

    cleaned export parallel corpus; discards date based retrieval

--- webapp/cli/export-parallel-corpus.py
+++ webapp/cli/export-parallel-corpus.py
@@ -1,24 +1,17 @@
 import os
-import sys
-import matplotlib.pyplot as plt
 import numpy as np
 from io import StringIO
 from tqdm import tqdm
 from argparse import ArgumentParser
-from sqlalchemy import func, and_
+from sqlalchemy import func, and_, or_
 
-from ilmulti.segment import Segmenter
-from ilmulti.sentencepiece import SentencePieceTokenizer
-from ilmulti.translator.pretrained import mm_all
+from ilmulti.translator import from_pretrained
 
-from webapp import db
-from webapp.models import Entry, Link, Translation, Retrieval
+from .. import db
+from ..models import Entry, Link, Translation, Retrieval
+from .utils import Preproc, ParallelWriter
 
-from bleualign.align import Aligner
-from cli.utils import Preproc, ParallelWriter
 from tools.align import BLEUAligner
-
-from cli import ILMULTI_DIR
 
 def get_datelinks(entry):
     links = []
@@ -54,23 +47,18 @@ def get_tgt_io(retrieved_id):
     tgt_tokenized, tgt_io = preproc.process(tgt.content, tgt.lang)
     return tgt_io
 
-def paralle_write(src_entry, src_lang, tgt_entry, tgt_lang, q_id, r_id):
-    #print('##########Article {} #########'.format(q_id),file=src_file)
-    print(src_entry,file=src_file)
-    #print('##########Article {} #########'.format(r_id),file=tgt_file)
-    print(tgt_entry,file=tgt_file)
+def paralle_write(src_entry, tgt_entry, query_id, retrieved_id):
+    print('{} {}'.format(query_id, retrieved_id), file=aligned)
+    print(src_entry, file=src_file)
+    print(tgt_entry, file=tgt_file)
 
-
-
-def align(score, threshold, src_io, tgt_io, hyp_io, q, r): 
+def align(src_io, tgt_io, hyp_io, query_id, retrieved_id): 
     src_aligned, tgt_aligned = aligner.bleu_align(src_io, tgt_io, hyp_io)
     src_aligned = preproc.detok(src_aligned)
     tgt_aligned = preproc.detok(tgt_aligned)               
     src_entry = '\n'.join(src_aligned)
     tgt_entry = '\n'.join(tgt_aligned)
-    paralle_write(src_entry, src_lang, tgt_entry, \
-                    tgt_lang, q, r)
-
+    paralle_write(src_entry, tgt_entry, query_id, retrieved_id)
 
 def calculate_threshold(scores):
     mean = np.mean(scores)
@@ -79,11 +67,10 @@ def calculate_threshold(scores):
     threshold = mean
     return threshold
 
-
 def export(src_lang, tgt_lang, model):
     entries = Entry.query.filter(
                 Entry.lang==src_lang
-                ).all()
+              ).all()
 
     entry_list = [entry.id for entry in entries]
     retrieved = Retrieval.query.filter(
@@ -94,8 +81,9 @@ def export(src_lang, tgt_lang, model):
                 ).all()
     scores = [r.score for r in retrieved if r]     
     threshold = calculate_threshold(scores)
+
     for entry in tqdm(entries):
-        date_links = get_datelinks(entry)
+        #date_links = get_datelinks(entry)
         src_io, hyp_io, exists = get_src_hyp_io(entry.id, tgt_lang, model)
         if exists:            
             retrieved = Retrieval.query.filter(
@@ -108,13 +96,13 @@ def export(src_lang, tgt_lang, model):
             if retrieved:
                 retrieved_id, score = retrieved.retrieved_id, retrieved.score
                 tgt_io = get_tgt_io(retrieved_id)
-                if retrieved_id in date_links:
-                    align(
-                        score, threshold, 
-                        src_io, tgt_io, hyp_io, 
-                        entry.id, retrieved_id
-                    )
-                elif score>=threshold:                    
+                # if retrieved_id in date_links:
+                #     align(
+                #         score, threshold, 
+                #         src_io, tgt_io, hyp_io, 
+                #         entry.id, retrieved_id
+                #     )
+                if score >= threshold:                    
                     align(
                         score, threshold, 
                         src_io, tgt_io, hyp_io, 
@@ -123,28 +111,20 @@ def export(src_lang, tgt_lang, model):
 
 if __name__ == '__main__':
     parser=ArgumentParser()
-    parser.add_argument('src_lang', help='non-english language')
-    parser.add_argument('tgt_lang', help='english is the target language')
+    parser.add_argument('--src_lang', help='source language, non-english', required=True)
+    parser.add_argument('--tgt_lang', help='target language', required=True )
+    parser.add_argument('--model', help='translation model for generating dataset', required=True)
     args = parser.parse_args()
+
     src_lang, tgt_lang = args.src_lang, args.tgt_lang
-    #fpath = os.getcwd()
-    #fname = 'pib_{}-{}'.format(tgt_lang, src_lang)
-    #pwriter = ParallelWriter(fpath, fname)
-    
-    langs = ['hi', 'ta', 'te', 'ml', 'ur', 'bn', 'gu', 'mr', 'pa', 'or']
+    model = args.model
 
-    segmenter = Segmenter()
-    tokenizer = SentencePieceTokenizer()
-    root = os.path.join(ILMULTI_DIR, 'mm-all')
-    translator = mm_all(root=root, use_cuda=True).get_translator()
-    aligner = BLEUAligner(translator, tokenizer, segmenter)
-    preproc = Preproc(segmenter, tokenizer)
+    engine = from_pretrained(tag=model, use_cuda=False)
+    aligner = BLEUAligner(engine.translator, engine.tokenizer, engine.segmenter)
+    preproc = Preproc(engine.segmenter, engine.tokenizer)
 
-
-    model = 'mm_all_iter1'
+    aligned = open('{}_aligned-{}-{}.txt'.format(model, src_lang, tgt_lang), 'a')
     src_file = open('pib_{}_en-{}.{}.txt'.format(model, src_lang, src_lang),'a')
     tgt_file = open('pib_{}_en-{}.{}.txt'.format(model, src_lang, tgt_lang),'a')
     
     export(src_lang, tgt_lang, model)
-
-
